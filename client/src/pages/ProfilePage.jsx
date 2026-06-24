e303d9ca7f2ab8bcab52598b3fa1944ab996b2df
rishu <rs9935422577@gmail.com>
2026-06-24 22:18:22 +0530

    something changed

--- client/src/pages/ProfilePage.jsx
+++ client/src/pages/ProfilePage.jsx
@@ -35,7 +35,7 @@ reader.onload = async ()=>{
             <label htmlFor="avatar" className='flex items-center gap-3 cursor-pointer'>
               <input onChange={(e)=>setSelectedImg(e.target.files[0])} type="file" id='avatar' accept='.png, .jpg, .jpeg' hidden />
               <img src={selectedImg ? URL.createObjectURL(selectedImg): assets.avatar_icon} alt="" className={`w-12 h-12 ${selectedImg && 'rounded-full'}`} />
-              upload profile image
+              upload profile here image
             </label>
             <input onChange={(e)=>setName(e.target.value)} value={name} type="text" required placeholder='Your name' className='p-2 border border-gray-500 rounded-md focus:outline-none focus:ring-2 focus:ring-violet-500' />
 
